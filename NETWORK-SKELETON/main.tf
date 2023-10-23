9bdb732ac6fdd4ed7016d092487d2f5dfa2ba6f2
sumitmoral <146178785+sumitmoral@users.noreply.github.com>
2023-10-23 11:12:45 +0530

    Update main.tf

--- NETWORK-SKELETON/main.tf
+++ NETWORK-SKELETON/main.tf
@@ -4,8 +4,9 @@ provider "aws" {
 
 
 module "network_skeleton" {
-  source               = "OT-CLOUD-KIT/network-skeleton/aws"
-  version              = "1.0.2"
+source = "./terraform-aws-network-skeleton/"
+#  source               = "OT-CLOUD-KIT/network-skeleton/aws"
+#  version              = "1.0.2"
   vpc_name             = var.vpc_name
   cidr_block           = var.cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
@@ -13,8 +14,11 @@ module "network_skeleton" {
   public_subnets_cidr  = var.public_subnets_cidr
   pvt_zone_name        = var.pvt_zone_name
   App_private_subnets_cidr = var.App_private_subnets_cidr
+  App_private_subnets_cidr1 = var.App_private_subnets_cidr1
   DB_private_subnets_cidr = var.DB_private_subnets_cidr
+  DB_private_subnets_cidr1 = var.DB_private_subnets_cidr1
   avaialability_zones  = var.avaialability_zones
+  avaialability_zones1 = var.avaialability_zones1
   logs_bucket          = var.logs_bucket
   logs_bucket_arn      = var.logs_bucket_arn
   tags                 = var.tags
